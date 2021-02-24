dcb4cde89d6b61e34303c6e4b346f33c509f579b
Mⱥlamali <56583866+Mlamalerie@users.noreply.github.com>
2021-02-24 17:19:36 +0100

    Update PrintArgs.java

--- PrintArgs.java
+++ PrintArgs.java
@@ -7,8 +7,17 @@ public class PrintArgs {
         // args c juste la listes de strings saisie par exemple "java Main.java beteau 0 pingpong"
         // la liste de d'argument yaura "bateau","0","pingpong"
        
+        // 9)
          System.out.println(args[0].toString()); // afficher le premier argument
       
+        // 10)
+        for (int i=0; i<args.length; i++)
+				System.out.println( (i+1)+" - "+ args[i]);
+        
+        // 11)
+        for(String name : args)
+				System.out.println(name.toString());
+        
         // exemple
         String [] names = { "Le", "langage", "Java", "me", "plait", "beaucoup"}; // liste de mot
 
