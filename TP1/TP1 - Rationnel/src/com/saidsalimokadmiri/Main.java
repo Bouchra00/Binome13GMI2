8f5521f4c5dd93cda44d0b490ec33dde4299c99d
Mlamali SAID SALIMO <saidsalimo@eisti.eu>
2021-03-03 14:22:59 +0100

    q5 mult

--- TP1/TP1 - Rationnel/src/com/saidsalimokadmiri/Main.java
+++ TP1/TP1 - Rationnel/src/com/saidsalimokadmiri/Main.java
@@ -1,11 +1,24 @@
 package com.saidsalimokadmiri;
 
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Rational a = new Rational(3,2);
-        System.out.println(a);
+        Rational a = new Rational(5,2);
+        System.out.println("a : " + a);
+        /*
+        Rational z = new Rational(3,0);
+        System.out.println(z); // Q3 : à ce stade ou on peut creer un rationnel de dénominateur égal à 0. C'est un pb
+        // parce que c'est impossible */
+        Rational y = new Rational(1,2);
+        System.out.println("y : " + y);
+        
+        // Q5
+        a.mult(y);
+        System.out.println("a*y : " + a);
+
+
 
     }
 }
--- TP1/TP1 - Rationnel/src/com/saidsalimokadmiri/Rational.java
+++ TP1/TP1 - Rationnel/src/com/saidsalimokadmiri/Rational.java
@@ -9,9 +9,21 @@ public class Rational {
     public Rational(int numerateur, int denominateur) {
         this.den = denominateur;
         this.num = numerateur;
+
+        if (this.den == 0) { // Q4 : si le dénominateur vaut 0, une erreur est donné, le programme s'arrete
+            throw new java.lang.Error("ZeroDivisionError");
+
+        }
+    }
+// Question 2
+    public String toString() {
+        return this.num + " / " + this.den;
     }
 
-    public String toString() {
-        return this.num + "/" + this.den;
+    // Question 5
+    public void mult(Rational f2) {
+        this.num *= f2.num;
+        this.den *= f2.den;
+
     }
 }
