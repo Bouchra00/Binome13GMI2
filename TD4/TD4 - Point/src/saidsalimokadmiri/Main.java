c71424b1aac21c2ac9da4ee524860e97a6598302
Mlamali SAID SALIMO <saidsalimo@eisti.eu>
2021-03-05 09:02:51 +0100

    begin Td4

---
+++ TD4/TD4 - Point/src/saidsalimokadmiri/Main.java
@@ -0,0 +1,11 @@
+package saidsalimokadmiri;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Point p = new Point();
+        System.out.println(p.x); // A.1 : x has private acces
+
+    }
+}
---
+++ TD4/TD4 - Point/src/saidsalimokadmiri/Point.java
@@ -0,0 +1,7 @@
+package saidsalimokadmiri;
+
+public class Point {
+
+    private int x;
+    private int y;
+}
